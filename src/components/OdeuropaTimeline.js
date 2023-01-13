7cd7362156777407626b727f01a442374ad43456
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2023-01-13 12:43:47 +0100

    OdeuropaTimeline: Add clear button

--- src/components/OdeuropaTimeline.js
+++ src/components/OdeuropaTimeline.js
@@ -1,5 +1,8 @@
 import { useState, useRef } from 'react';
 import styled, { css } from 'styled-components';
+import { useTranslation } from 'next-i18next';
+
+import Button from '@components/Button';
 
 const Container = styled.div`
   width: 100%;
@@ -73,6 +76,31 @@ const Item = styled.li`
   }
 `;
 
+const CrossIcon = (props) => (
+  <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" {...props}>
+    <path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z" />
+  </svg>
+);
+
+const ClearButton = styled(Button)`
+  cursor: pointer;
+  border-radius: 30px;
+
+  position: absolute;
+  top: 12px;
+  left: 12px;
+  z-index: 10;
+  background-color: transparent;
+  color: #000;
+
+  &:hover {
+    color: #000;
+    border-radius: 0;
+    box-shadow: none;
+    text-decoration: underline;
+  }
+`;
+
 const calculateSelectionBox = (startPoint, endPoint) => ({
   left: Math.min(startPoint.x, endPoint.x),
   top: Math.min(startPoint.y, endPoint.y),
@@ -95,6 +123,7 @@ const OdeuropaTimeline = ({
   onChange,
   ...props
 }) => {
+  const { t } = useTranslation(['search']);
   const [activeItems, setActiveItems] = useState(defaultValues);
   const [dragStart, setDragStart] = useState(null);
   const [dragEnd, setDragEnd] = useState(null);
@@ -249,6 +278,10 @@ const OdeuropaTimeline = ({
     onChange?.(values);
   };
 
+  const handleOnClear = () => {
+    handleOnChange([]);
+  };
+
   return (
     <Container
       {...props}
@@ -259,6 +292,11 @@ const OdeuropaTimeline = ({
       onMouseMove={handleMouseEvent}
       onTouchMove={handleMouseEvent}
     >
+      {defaultValues.length > 0 && (
+        <ClearButton primary onClick={handleOnClear}>
+          <CrossIcon /> {t('search:buttons.clear')}
+        </ClearButton>
+      )}
       <Chart>
         {histogram}
         <Line />
