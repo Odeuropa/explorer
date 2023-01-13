d2f0e5d38b79b2a9a0a862cbd0b4ce153d0d3b37
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2023-01-13 15:24:30 +0100

    List details page: Fix hydration issue with dates

--- src/pages/lists/[listId].js
+++ src/pages/lists/[listId].js
@@ -132,11 +132,11 @@ function ListsPage({ isOwner, list, shareLink }) {
                       <time
                         key="0"
                         dateTime={new Date(list.created_at).toISOString()}
-                        title={new Date(list.created_at).toString()}
+                        title={new Date(list.created_at).toISOString()}
                       />,
                     ]}
                     values={{
-                      date: new Date(list.created_at).toLocaleDateString(),
+                      date: new Date(list.created_at).toLocaleDateString(i18n.language),
                     }}
                   />
                 </p>
@@ -147,11 +147,11 @@ function ListsPage({ isOwner, list, shareLink }) {
                       <time
                         key="0"
                         dateTime={new Date(list.updated_at).toISOString()}
-                        title={new Date(list.updated_at).toString()}
+                        title={new Date(list.updated_at).toISOString()}
                       />,
                     ]}
                     values={{
-                      date: new Date(list.updated_at).toLocaleDateString(),
+                      date: new Date(list.updated_at).toLocaleDateString(i18n.language),
                     }}
                   />
                 </p>
